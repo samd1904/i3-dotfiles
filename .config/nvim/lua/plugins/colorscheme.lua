a46c352028641a94c32f6d94fc642d17bdce4b3a
Saumya Darne <saumya@simplifix.systems>
2025-02-17 13:08:44 +0530

    colorschemes for nvim added

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  { "EdenEast/nightfox.nvim" },
+
+  { "Skardyy/makurai-nvim" },
+
+  { "bluz71/vim-moonfly-colors" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "moonfly",
+    },
+  },
+}
